72d78cf56fbd34f39816fdf1e1354d3bed3891b1
Ivan Franjic <2338268+frennky@users.noreply.github.com>
2020-11-01 17:48:54 +0100

    added some unit tests

---
+++ src/test/java/com/me/sonarqubeexample/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.me.sonarqubeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloControllerTest {
+
+    @Autowired
+    private HelloController controller;
+
+    @Test
+    public void testContextLoads() throws Exception {
+        assertNotNull(controller);
+    }
+
+    @Test
+    void testIndex() {
+        assertEquals(controller.greeting, controller.index());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("Hello Dave\n", controller.greet("Dave"));
+    }
+}
